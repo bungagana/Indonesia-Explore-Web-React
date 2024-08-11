987f3cba831341c97046ec3dfe088c599a349878
bungagana <bungamagelang57@gmail.com>
2024-08-11 20:26:12 +0700

    add game

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,7 @@ import './css/Game.css'; // For styling
 const Game = () => {
     useEffect(() => {
         // Redirect to the external URL
-        window.location.href = 'https://chic-empanada-8387a6.netlify.app/';
+        window.location.href = 'https://66b8bb8fb4d6405c61245faf--ephemeral-phoenix-a5016c.netlify.app/';
     }, []);
 
     return null; // Optionally, you can display a loading message or spinner here
