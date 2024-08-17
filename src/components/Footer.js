9bceec07ab38bb09681193ff15b4cb0670490360
bungagana <bungamagelang57@gmail.com>
2024-08-17 17:12:58 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 MerdekaHack. Semua hak cipta dilindungi.</p>
+      <p>&copy; 2024 AI IMPACT Semua hak cipta dilindungi.</p>
     </footer>
   );
 }
